ee17e464f967c64616dff3736ba4e76fbc6453f2
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-30 02:49:51 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
 end
+=======
+class Student < ActiveRecord::Base 
+  def to_s 
+    self.first_name + " " + self.last_name 
+  end 
+end 
+
+>>>>>>> 91c2fae6e496b57210779a60813fa82d8dc4d3df
--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,4 +1,5 @@
 class CreateStudents < ActiveRecord::Migration
+<<<<<<< HEAD
   def change
     create_table :students do |t|
       t.string :first_name
@@ -7,3 +8,13 @@ class CreateStudents < ActiveRecord::Migration
     end
   end
 end
+=======
+  def change 
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name 
+      t.timestamps null: false 
+    end 
+  end 
+end 
+>>>>>>> 91c2fae6e496b57210779a60813fa82d8dc4d3df
